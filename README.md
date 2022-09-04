ac347cff8ad21c00fdac8e14fe9d248c3e86761a
Isira Adithya <rp.isira@gmail.com>
2022-09-04 12:28:12 +0530

    Pushing the local code to the github

---
+++ README.md
@@ -0,0 +1,30 @@
+## Burp Collaborator to Discord
+This tool will poll a Burp Collaborator domain's client interactions and will notify those interactions to the user through Discord Webhooks.
+
+## Requirements
+This script requires the `requests` library - https://pypi.org/project/requests/.
+Install it by running `pip install requests`.
+
+## Setup
+- Install Wireshark
+- Start capturing the data packets from your network adapter
+- Start Burpsuite
+- Visit `Project Options` -> `Misc` and then tick the `Poll over unencrypted HTTP` option
+- ![](https://i.imgur.com/PLNTkQu.png)
+- Open the Burp Collaborator and click `Copy to clipboard` then click `Poll now`
+- ![](https://i.imgur.com/1ssZ1nX.png)
+- Make sure to notedown the collaborator domain somewhere or just add the domain to the value of `cdomain` in `./settings.json`
+- Now, check the wireshark and filter the HTTP packets
+- Find, HTTP request(s) to `http://polling.oastify.com/burpresults` or `https://polling.burpcollaborator.net/burpresults` *(Old versions of burpsuite will use this one)*
+- Extract the `Collaborator URL` and the `BIID` from the HTTP request as shown below.
+- ![](https://i.imgur.com/73mm0XS.png)
+- Paste the value of `Collaborator URL` in `polling-endpoint` key in `./settings.json`
+- Paste the value of `BIID` in `biid` key in `./settings.json`
+- Now, create a discord server and create a new text channel.
+- Under channel settings, goto `Integrations` and create a new webhook.
+- ![](https://i.imgur.com/CHZp20u.png)
+- Now copy the webhook URL and then paste the webhook URL inside the `dWebhook` key in `./settings.json`
+- Finally, your `./settings.json` should be completed and the script is ready to be executed. 
+
+## Templates
+You can customize your discord message templates by editing `txt` files under the `templates` folder.
---
+++ main.py
@@ -0,0 +1,77 @@
+from email.mime import base
+import requests
+import json
+import base64
+
+# Reading the settings.json file
+try:
+    _settings = json.load(open('settings.json', 'r'))
+except Exception as e:
+    print("[ERR] Cannot read the 'settings.json' file.")
+    pass
+
+# Template Generators
+def getHTTPMessageFromTemplate(cResponse):
+    templateString = ""
+    with open('./templates/http.txt', 'r') as wF:
+        templateString = templateString = wF.read()
+        templateString = templateString.replace("{{type}}", "HTTP")
+        templateString = templateString.replace("{{protocol}}", cResponse['protocol'])
+        templateString = templateString.replace("{{from}}", cResponse['client'])
+        templateString = templateString.replace("{{time}}", cResponse['time'])
+        templateString = templateString.replace("{{request}}", base64.b64decode(cResponse['data']['request']).decode('utf-8'))
+        templateString = templateString.replace("{{response}}", base64.b64decode(cResponse['data']['response']).decode('utf-8'))
+    return templateString
+
+def getDNSMessageFromTemplate(cResponse):
+    templateString = ""
+    with open('./templates/dns.txt', 'r') as wF:
+        templateString = wF.read()
+        templateString = templateString.replace("{{type}}", "DNS")
+        templateString = templateString.replace("{{from}}", cResponse['client'])
+        templateString = templateString.replace("{{domain}}", cResponse['data']['subDomain'])
+        templateString = templateString.replace("{{time}}", cResponse['time'])
+        try:
+            templateString = templateString.replace("{{request}}", base64.b64decode(cResponse['data']['rawRequest']).decode('utf-8'))
+        except Exception as e:
+            templateString = templateString.replace("{{request}}", cResponse['data']['rawRequest'])
+    return templateString
+
+def getSMTPMessageFromTemplate(cResponse):
+    templateString = ""
+    with open('./templates/smtp.txt', 'r') as wF:
+        templateString = wF.read()
+        templateString = templateString.replace("{{type}}", "SMTP")
+        templateString = templateString.replace("{{sender}}", cResponse['data']['sender'])
+        templateString = templateString.replace("{{time}}", cResponse['time'])
+        templateString = templateString.replace("{{recipients}}", base64.b64decode(cResponse['data']['recipients']).decode('utf-8'))
+        templateString = templateString.replace("{{message}}", base64.b64decode(cResponse['data']['message']).decode('utf-8'))
+        templateString = templateString.replace("{{conversation}}", base64.b64decode(cResponse['data']['conversation']).decode('utf-8'))
+    return templateString
+
+# Discord Message Sender
+def sendToDiscord(message):
+    data = {
+        "content" : message,
+        "username" : "CustomUsername"
+    }
+    requests.post(_settings['dWebhook'], json=data)
+    
+
+# Checking if the given information is correct
+res = requests.post(f"https://{_settings['cdomain']}/bcollabtodiscord")
+if (res.status_code == 200):
+    resContent = res.content.decode()
+    res = requests.get(f"{_settings['polling-endpoint']}?biid={_settings['biid']}")
+    if (res.status_code == 200):
+        cResults = json.loads(res.content.decode())
+        for cResponse in cResults['responses']:
+            if (cResponse['protocol'] == 'https' or cResponse['protocol'] == 'http'):
+                message = getHTTPMessageFromTemplate(cResponse)
+            elif (cResponse['protocol'] == 'dns'):
+                message = getDNSMessageFromTemplate(cResponse)
+            elif (cResponse['protocol'] == 'smtp'):
+                message = getSMTPMessageFromTemplate(cResponse)
+            else:
+                message = json.dumps(cResponse)
+            sendToDiscord(message=message)
